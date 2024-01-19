1e2ad740c4600a5a55e411da8c8a89ea295bf71e
Pedro Falco <fpfalco@gmail.com>
2024-01-18 22:46:28 -0300

    final version

---
+++ src/lib/translations.js
@@ -0,0 +1,20 @@
+export default {
+    en: {
+        "invite.invite": "Invite",
+        "invite.begin": "Scroll to begin",
+        "invite.drop": "Drop",
+        "invite.p1": "ArtDAO has been working for the past few months on our own marketplace infrastructure on the Fuel platform, a new Layer-2 that expands from Ethereum. We believe this offers a powerful new opportunity to expand the possibilities of digital art on Ethereum - connecting new people to artists and creating a new kind of marketplace built by and for artists and their communities.",
+        "invite.p2": "To do this, we're putting together a new series of curated releases, launching new 1/1 works alongside an online magazine, a curatorial concept and a physical exhibition.",
+        "invite.p3": "For Hyperescapism, we are working with Femzor & Occulted to explore the idea of escapism, examining how transversal forms of digital technology are caught between the possibility of sublimating our imaginations in corporate marketing campaigns and, on the other hand, as a means of digression, subterfuge and creation: a reimagining of the possible.",
+        "invite.p4": "We would like to invite you to create a limited edition with us, alongside 20 other artists selected by the curators.",
+    },
+    pr: {
+        "invite.invite": "Convite",
+        "invite.begin": "Percorrer para começar",
+        "invite.drop": "Drop",
+        "invite.p1": "O ArtDAO tem trabalhado nos últimos meses em nossa própria infraestrutura de mercado na plataforma Fuel, uma nova Layer-2 que se expande a partir do Ethereum. Acreditamos que isso oferece uma nova e poderosa oportunidade para ampliar as possibilidades da arte digital no Ethereum - conectando novas pessoas aos artistas e criando um novo tipo de mercado construído por e para os artistas e suas comunidades.",
+        "invite.p2": "Para fazer isso, estamos reunindo uma nova série de lançamentos curados, lançando novas obras 1/1 junto com uma revista online, um conceito de curadoria e uma exposição física.",
+        "invite.p3": "Para o Hyperescapismo, estamos trabalhando com Femzor & Occulted para explorar a ideia de escapismo, examinando como formas transversais de tecnologia digital estão presas entre a possibilidade de sublimação de nossas imaginações nas campanhas de marketing corporativo e, por outro lado, como meio de digressão, subterfúgio e criação: uma reimaginação do possível.",
+        "invite.p4": "Gostaríamos de convidá-lo a criar uma edição limitada conosco, ao lado de outros 20 artistas selecionados pelos curadores.",
+    },
+  };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,55 @@
 <script>
-    // create a simple three.js sketch on svelte page
-
-    import { onMount } from 'svelte';
-    import * as THREE from 'three';
     import '/src/style.css';
+    import { onMount } from 'svelte';
+    import { t, locale, locales } from "$lib/i18n";
 
-    onMount(() => {
+    $locale = 'en';
 
-        return () => {
-        };
-    });
 </script>
 
-
-<div class="gradient-down">
-    <img class="w-full h-full lp:h-full" src="/title.png" alt="">
+<div class="gradient-down -z-10 fixed w-screen top-0 left-0">
+    <img class="w-full h-fit" src="/background-full.png" alt="">
 </div>
 
-<div class="select-none absolute bottom-1/2 lp:bottom-0 left-1/2 -translate-x-1/2 text-center mb-3 lp:mb-6">
-    <h1 class="uppercase text-2xl lp:text-5xl">invitee</h1>
-    <p class="text-sm lp:text-lg my-2 lp:my-4 tracking-[3px]">scroll to begin</p>
-    <img class="mx-auto scale-75 lp:scale-100" src="/arrow.svg" alt="Scroll to begin">
-</div>
-
-<div class=" lp:text-xl text-center lp:w-1/2 mx-4 lp:mx-auto tracking-wider leading-8 ">
-    <h1 class="uppercase text-3xl lp:text-5xl text-center mt-12 mb-8 lp:mt-32 lp:mb-16" >drop</h1>
-    <div>
-        <p>
-            O ArtDAO tem trabalhado nos últimos meses em nossa própria infraestrutura de mercado na plataforma Fuel, uma nova Layer-2 que se expande a partir do Ethereum. Acreditamos que isso oferece uma nova e poderosa oportunidade para ampliar as possibilidades da arte digital no Ethereum - conectando novas pessoas aos artistas e criando um novo tipo de mercado construído por e para os artistas e suas comunidades.
-        </p>
-        <br>
-        <p>
-            Para fazer isso, estamos reunindo uma nova série de lançamentos curados, lançando novas obras 1/1 junto com uma revista online, um conceito de curadoria e uma exposição física.
-        </p>
-        <br>
-        <p>
-            Para o Hyperescapismo, estamos trabalhando com Femzor & Occulted para explorar a ideia de escapismo, examinando como formas transversais de tecnologia digital estão presas entre a possibilidade de sublimação de nossas imaginações nas campanhas de marketing corporativo e, por outro lado, como meio de digressão, subterfúgio e criação: uma reimaginação do possível.
-        </p>
-        <br>
-        <p class=" text-red-500 ">
-            Gostaríamos de convidá-lo a criar uma edição limitada conosco, ao lado de outros 20 artistas selecionados pelos curadores.
-        </p>
-    </div>
-
-    <h1 class="uppercase text-3xl lp:text-5xl text-center mt-12 mb-8 lp:mt-32 lp:mb-16" >exhibition</h1>
-
-    <div>
-        <p>
-            Para lançar o lançamento e a revista online, estaremos realizando uma exposição temporária em São Paulo no 2024.
-        </p>
-        <br>
-        <p>
-            Estamos trabalhando com Femzor & Occulted para transformar um espaço em uma contraparte física temporária do mundo online do Hyperescapismo.
-        </p>
+<div class="h-screen w-full flex flex-col items-center justify-between">
+    <div class="text-base my-6 flex justify-center items-center min-w-full gap-3">
+        <button class="{$locale == "pr" ? "opacity-100" :"opacity-30"}" on:click={()=> $locale = 'pr'}>PR</button>
+        <span class="opacity-30">/</span>
+        <button class="{$locale == "en" ? "opacity-100" :"opacity-30"}" on:click={()=> $locale = 'en'}>EN</button>
     </div>
     
-    <p class="text-xs lp:text-base uppercase underline text-red-500">full terms & conditions</p>
-    
-    <div class="text-xs lp:text-lg font-semibold border-[1px] my-10 lp:my-20 shadow-md rounded-md uppercase px-14 py-2 w-fit mx-auto border-black">
-        <a href={"#"}>join us</a>
+    <div class="lp:scale-50">
+        <img class="w-full h-full lp:h-full" src="/title.png" alt="">
+    </div>
+
+    <div class="gradient-down-balls origin-top scale-50 lp:scale-100 lp:-translate-y-1/2">
+        <img class="w-full h-full lp:h-full" src="/balls.png" alt="">
+    </div>
+
+    <div class="select-none text-center mb-2">
+        <h1 class="uppercase text-2xl lp:text-4xl">{$t("invite.invite")}</h1>
+        
+        <p class="text-sm lp:text-lg my-2 lp:my-2 tracking-[3px]">{$t("invite.begin")}</p>
+        <img class="mx-auto scale-75 lp:scale-100" src="/arrow.svg" alt="Scroll to begin">
     </div>
 </div>
 
-<div class="gradient-up">
-    <img class="w-fullh-full lp:h-full" src="/footer.png" alt="">
+<div class="lp:text-xl text-center lp:w-1/3 mx-12 lp:mx-auto tracking-wider">
+    <h1 class="uppercase text-3xl lp:text-5xl text-center mt-28 mb-8 lp:mt-22 lp:mb-16 ">{$t("invite.drop")}</h1>
+    <div class="leading-7">
+        <p>{$t("invite.p1")}</p>
+        <br>
+        <p>{$t("invite.p2")}</p>
+        <br>
+        <p>{$t("invite.p3")}</p>
+        <br>
+        <p class=" text-red-500 ">{$t("invite.p4")}</p>
+    </div>
+    <div class="scale-[65%] lp:scale-75">
+        <a href={"#"}><img class="mx-auto" src="/join.png" alt=""></a>
+    </div>
+</div>
+
+<div class="gradient-up w-screen -z-10 fixed bottom-0 left-0">
+    <img class="w-full h-fit" src="/background-bottom.png" alt="">
 </div>
